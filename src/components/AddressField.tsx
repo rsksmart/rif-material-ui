900e9e8e17c4c785744033897834253a6ac05a30
Luis Chavarría <luis.chavarria@iovlabs.org>
2021-09-29 22:21:04 -0600

    feat: add address field

---
+++ src/components/AddressField.tsx
@@ -0,0 +1,50 @@
+
+import React, { useState } from 'react'
+import { makeStyles } from '@mui/material/styles'
+import { TextField } from '@mui/material'
+import Resolver from '@rsksmart/rns-resolver.js'
+
+// @ts-ignore
+// eslint-disable-next-line new-cap
+const resolver = new Resolver.forRskTestnet()
+
+export const AddressField = ({ receiverAddress, onReceiverAddressChange } : any) => {
+  const [to, setTo] = useState('')
+  const [resolverStatus, setResolverStatus] = useState('')
+  const rskDomainValidation = (e:any) => {
+    const selectedTo = (e && e.target && e.target.value)
+    setTo(selectedTo)
+    if (!selectedTo) {
+      return
+    }
+
+    const re = /\.rsk$/ // match *.rsk domains
+    const isValid = re.test(String(selectedTo).toLowerCase())
+
+    if (!selectedTo) {
+      setResolverStatus('')
+    } else if (!isValid) {
+      onReceiverAddressChange(selectedTo)
+      setResolverStatus('')
+    } else {
+      setResolverStatus('Fetching address...')
+      resolver.addr(selectedTo).then((address:string) => {
+        onReceiverAddressChange(address)
+        setResolverStatus(address)
+      }).catch((e:any) => {
+        setResolverStatus('Domain with no address')
+      }) // gets rs
+    }
+  }
+  return (
+    <div>
+      <TextField id="to" label="To" value={to} onChange={rskDomainValidation} autoComplete='off'/>
+  <div style={{
+    marginTop: '5px',
+    color: 'white',
+    fontSize: '18px',
+    fontWeight: 'bold'
+  }} className={'resolverStatus'}>{resolverStatus}</div>
+    </div>
+  )
+}
---
+++ src/stories/AddressField.stories.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { ComponentStory, ComponentMeta } from '@storybook/react'
+
+import themeDecorator from './decorators'
+import { AddressField } from '../components/AddressField'
+
+export default {
+  title: 'Components/TextField',
+  component: AddressField,
+  parameters: {
+    docs: {
+      page: null
+    }
+  },
+  decorators: [themeDecorator]
+} as ComponentMeta<typeof AddressField>
+
+const Template: ComponentStory<typeof AddressField> = (args) => <AddressField {...args} />
+
+export const Primary = Template.bind({})
+Primary.args = {
+  children: 'TextBu',
+  label: 'To',
+  onReceiverAddressChange: () => {}
+}
+
+export const Secondary = Template.bind({})
+Secondary.args = {
+  color: 'secondary',
+  onReceiverAddressChange: () => {}
+}
